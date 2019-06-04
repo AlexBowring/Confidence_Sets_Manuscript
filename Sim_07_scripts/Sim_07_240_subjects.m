84d4a75c89ac162191a657f0016aa7730868c27c
AlexBowring <A.bowring@warwick.ac.uk>
2019-06-04 20:21:36 +0100

    All 2D simulation scripts

---
+++ Sim_07_scripts/Sim_07_240_subjects.m
@@ -0,0 +1,10 @@
+nSubj=240;
+nRlz=30;
+
+cd /storage/maullz/Confidence_Sets_Manuscript/
+addpath('/storage/essicd/spm8/')
+addpath(genpath('/storage/maullz/Confidence_Sets_Manuscript/'))
+
+rng('shuffle')
+tID=str2num(getenv('SGE_TASK_ID'))
+Sim_07(nSubj,['Sim_07_' num2str(nSubj) '_subjects_' sprintf('%03d',tID)],nRlz)
---
+++ Sim_11_scripts/Sim_11_240_subjects.m
@@ -0,0 +1,10 @@
+nSubj=240;
+nRlz=30;
+
+cd /storage/maullz/Confidence_Sets_Manuscript/
+addpath('/storage/essicd/spm8/')
+addpath(genpath('/storage/maullz/Confidence_Sets_Manuscript/'))
+
+rng('shuffle')
+tID=str2num(getenv('SGE_TASK_ID'))
+Sim_11(nSubj,['Sim_11_' num2str(nSubj) '_subjects_' sprintf('%03d',tID)],nRlz)
