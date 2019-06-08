bd2a409797848950ee535f5e2875d2dd37f97a13
AlexBowring <A.bowring@warwick.ac.uk>
2019-06-08 18:29:11 +0100

    Added multi sphere signal to create_signal.m

--- custom_functions/create_signal.m
+++ custom_functions/create_signal.m
@@ -9,6 +9,7 @@ function Y = create_signal(dim, signal_type, param, smo, trnind)
 %							is a ramp from 0 to X, or a vector [X,Y], in which case the signal a ramp from X to Y in the x direction.
 %							If 'signal_type' is 'circle', then param is a vector [M, R], where M is the magnitude of the signal and R is the radius.
 %							If 'signal_type' is 'sphere', then param is a vector [M, R], where M is the magnitude of the signal and R is the radius.
+%                           If 'signal_type' is 'multi', then param is a value M, which is the magnitude of the three spherical signals created. 
 %       smo:                A value X giving the smoothing FWHM for isotropic smoothing
 %       trnind: 			A cell array for truncating the signal back to original dimensions
 %
@@ -49,6 +50,18 @@ switch(signal_type)
         % Truncate to avoid edge effects
         tSigs          = Sigs(trnind{1}, trnind{2}, trnind{3});
         maxtSigs       = max(tSigs(:));
-        Y              = (param(1)/maxtSigs)*tSigs;     
+        Y              = (param(1)/maxtSigs)*tSigs;    
+    case 'multi'
+        % Create smooth spherical signal and truncates, therefore wdim
+        % must be entered as dim
+        Sig = imtranslate(SpheroidSignal(dim, 20, param, 0),[-10,-25]) + imtranslate(SpheroidSignal(dim, 15, param, 0),[20, -10]) + imtranslate(SpheroidSignal(dim, 10, param, 0),[25, 13]) + ...
+              imtranslate(SpheroidSignal(dim, 18, param, 0),[-15,25]);
+        % Smoothing the signal
+        Sigs = zeros(dim);
+        ss   = spm_smooth(Sig,Sigs,smo*ones(1,3));
+        % Truncate to avoid edge effects
+        tSigs          = Sigs(trnind{1}, trnind{2}, trnind{3});
+        maxtSigs       = max(tSigs(:));
+        Y              = (param/maxtSigs)*tSigs;   
 end
 
