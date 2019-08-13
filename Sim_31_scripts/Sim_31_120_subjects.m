6f4fad8dadbe7be1d46c368b14c509a3bba55512
AlexBowring <A.bowring@warwick.ac.uk>
2019-08-13 14:55:37 +0100

    Simulation scripts for Biobank signal

---
+++ Sim_31_scripts/Sim_31_120_subjects.m
@@ -0,0 +1,10 @@
+nSubj=120;
+nRlz=5;
+
+cd /storage/maullz/Confidence_Sets_Manuscript/
+addpath('/storage/essicd/spm8/')
+addpath(genpath('/storage/maullz/Confidence_Sets_Manuscript/'))
+
+rng('shuffle')
+tID=str2num(getenv('SGE_TASK_ID'))
+Sim_31(nSubj,['Sim_31_' num2str(nSubj) '_subjects_' sprintf('%03d',tID)],nRlz)
---
+++ Sim_32_scripts/Sim_32_240_subjects.m
@@ -0,0 +1,10 @@
+nSubj=240;
+nRlz=5;
+
+cd /storage/maullz/Confidence_Sets_Manuscript/
+addpath('/storage/essicd/spm8/')
+addpath(genpath('/storage/maullz/Confidence_Sets_Manuscript/'))
+
+rng('shuffle')
+tID=str2num(getenv('SGE_TASK_ID'))
+Sim_32(nSubj,['Sim_32_' num2str(nSubj) '_subjects_' sprintf('%03d',tID)],nRlz)
