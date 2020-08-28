862a936421cfe04036edec92338ed065a04c8f07
Alex Bowring <alex.bowring@bdi.ox.ac.uk>
2020-08-28 01:43:52 +0100

    debug, replaced nans with 0 in union and intersectionmaps

--- Biobank_simulation/Algorithm_1_scripts/Algorithm_1.m
+++ Biobank_simulation/Algorithm_1_scripts/Algorithm_1.m
@@ -189,8 +189,8 @@ for t=1:nRlz
       observed_std = observed_std.*intersection_mask;
       observed_cohen_d = observed_cohen_d.*intersection_mask;
       
-      intersection_of_ground_truths = intersection_of_ground_truths.*reshape(intersection_mask,dim);
-      union_of_ground_truths = union_of_ground_truths.*reshape(intersection_mask,dim);
+      intersection_of_ground_truths = intersection_of_ground_truths.*reshape(intersection_mask,dim) > 0;
+      union_of_ground_truths = union_of_ground_truths.*reshape(intersection_mask,dim) > 0;
       
       % ... and we only want to consider the ground truth in the mask too
       cohen_d = cohen_d_unmasked.*reshape(intersection_mask, dim);
